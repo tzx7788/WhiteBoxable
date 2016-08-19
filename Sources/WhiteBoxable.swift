42e80bbb862582779662972b7a45d12697a132e6
Tang Zhixiong <tzx7788@me.com>
2016-08-19 22:35:52 +0800

    update

--- Sources/WhiteBoxable.swift
+++ Sources/WhiteBoxable.swift
@@ -4,10 +4,10 @@ public protocol WhiteBoxable {
     
     var outputLength: Int { get }
     var name: String { get }
-    func value(input: UInt8) -> [UInt8]
+    func value(_ input: UInt8) -> [UInt8]
 }
 
-public func cfunc<T: WhiteBoxable>(whitebox: T) -> String {
+public func cfunc<T: WhiteBoxable>(_ whitebox: T) -> String {
     let name = whitebox.name
     var result = "static void \(name)(uint8_t input, uint8_t* output) {\n"
     result.append("\(cVariable(name:"data", whitebox: whitebox))\n")
@@ -22,7 +22,7 @@ public func cVariable< T: WhiteBoxable>(name n: String? = nil,whitebox: T) -> St
     for index in 0...UInt8.max {
         result = result + "/* \(String(format: "0x%02x", index)) */\t"
         let input = index
-        let array = whitebox.value(input: input).map{String(format:"0x%02x", $0)}
+        let array = whitebox.value(input).map{String(format:"0x%02x", $0)}
         let string = array.reduce("") {
             "\($0)\($1), "
         }
